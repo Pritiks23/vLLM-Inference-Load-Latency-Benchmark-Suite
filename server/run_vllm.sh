5f7eb538ae6e749fffb47350cd8d65e7684b899e
Pritika Vipin <65793273+Pritiks23@users.noreply.github.com>
2026-06-08 13:40:47 -0700

    Update run_vllm.sh

--- server/run_vllm.sh
+++ server/run_vllm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 python -m vllm.entrypoints.openai.api_server \
-  --model mistralai/Mistral-7B-Instruct-v0.2 \
+  --model microsoft/Phi-3-mini-4k-instruct \
   --host 0.0.0.0 \
   --port 8000 \
-  --gpu-memory-utilization 0.75 \
-  --max-model-len 2048
+  --gpu-memory-utilization 0.50 \
+  --max-model-len 1025
